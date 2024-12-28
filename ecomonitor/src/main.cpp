93fa1c71d8da04deb665e8b3510d0a1895a938c7
nullsploit01 <harshaldharmik099@gmail.com>
2024-12-28 23:09:14 +0530

    add ability to read humidity and temperature

--- ecomonitor/src/main.cpp
+++ ecomonitor/src/main.cpp
@@ -1,26 +1,46 @@
 
 #include "Arduino.h"
+#include "DHT.h"
 
+#define DHTPIN D2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   pinMode(D3, OUTPUT);
-  pinMode(D2, INPUT);
 
   Serial.begin(9600);
+  dht.begin();
 }
 
 void loop()
 {
-  int detection = digitalRead(D2);
 
-  if(detection == HIGH) {
-    Serial.println("detected");
-    digitalWrite(D3, HIGH);
-  }else{
-    Serial.println("clear");
-    digitalWrite(D3, LOW);
+  // Read the humidity
+  float h = dht.readHumidity();
+  // Read temperature as Celsius 
+  float t = dht.readTemperature();
+
+  // Check if any reads have failed
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed reading from DHT sensor"));
+    return;
   }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  
+  Serial.print(F("\nHumedity: "));
+  Serial.print(h);
+  Serial.print(F("% "));
+  Serial.print(F("Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F("Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C"));
 
   delay(1550);
 }
