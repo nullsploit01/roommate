ad068c70d0e58d7717d279626c0bbe66aea74a92
nullsploit01 <harshaldharmik099@gmail.com>
2024-12-29 04:34:55 +0530

    create separate header file for dht reading

--- ecomonitor/src/main.cpp
+++ ecomonitor/src/main.cpp
@@ -6,48 +6,28 @@
 // Include the library code
 #include <Configuration.h>
 #include <ConnectWifi.h>
-
-#define DHTPIN D2
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
+#include <HumidityTemperatureReader.h>
 
 void setup()
 {
   Serial.begin(115200);
-  
-  connectWifi();
   pinMode(D3, OUTPUT);
 
+  connectWifi();
   dht.begin();
 }
 
 void loop()
 {
 
-  // Read the humidity
-  float h = dht.readHumidity();
-  // Read temperature as Celsius 
-  float t = dht.readTemperature();
-
-  // Check if any reads have failed
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed reading from DHT sensor"));
-    return;
+  if (isConnected())
+  {
+    readDHTValues();
   }
-
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-  
-  Serial.print(F("\nHumidity: "));
-  Serial.print(h);
-  Serial.print(F("% "));
-  Serial.print(F("Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(F("Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C"));
+  else
+  {
+    Serial.println("WiFi Disconnected");
+  }
 
   delay(5000);
 }
