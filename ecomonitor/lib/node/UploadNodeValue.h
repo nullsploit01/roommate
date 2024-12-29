a786e7f86fc17515310deeeb5988023f66d05a11
nullsploit01 <harshaldharmik099@gmail.com>
2024-12-30 02:47:02 +0530

    fix json for payload

--- ecomonitor/lib/node/UploadNodeValue.h
+++ ecomonitor/lib/node/UploadNodeValue.h
@@ -2,7 +2,7 @@
 
 void uploadNodeValue(String value, String API_KEY)
 {
-    String jsonData = "{\"value\": " + value + ", \"api_key\": " + API_KEY + "}";
+    String jsonData = "{\"value\": \"" + value + "\", \"api_key\": \"" + API_KEY + "\"}";
     String response = sendPostRequest("/nodes/value", jsonData);
     Serial.println(response);
 }
