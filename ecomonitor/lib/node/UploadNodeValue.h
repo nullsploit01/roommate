8fb20f05e323f9926370e0ccc7f0d021f30281a3
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-12 04:49:09 +0530

    add ability to program using python

--- ecomonitor/lib/node/UploadNodeValue.h
+++
@@ -1,8 +0,0 @@
-#include <ServerHttpClient.h>
-
-void uploadNodeValue(String value, String API_KEY)
-{
-    String jsonData = "{\"value\": \"" + value + "\", \"api_key\": \"" + API_KEY + "\"}";
-    String response = sendPostRequest("/nodes/value", jsonData);
-    Serial.println(response);
-}
