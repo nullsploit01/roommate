0c0893d50aa9815ba6db22cd38195984433623e0
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-12 23:17:24 +0530

    add abilty to connect to wifi

--- ecomonitor/main.py
+++ ecomonitor/main.py
@@ -1,7 +1,31 @@
 import dht
 import machine
 import time
+import config
+import network
 
+SSID = config.WIFI_SSID
+PASSWORD = config.WIFI_PASSWORD
+
+WIFI_LED_LIGHT = machine.Pin(0, machine.Pin.OUT) 
+
+def connect_wifi():
+	time.sleep(3)
+	wlan = network.WLAN(network.STA_IF)  
+	wlan.active(True)                  
+	wlan.connect(SSID, PASSWORD)        
+
+	print("Connecting to Wi-Fi...")
+	while not wlan.isconnected():       
+		time.sleep(1)
+		print(".", end="")
+		
+
+	print("\nConnected to Wi-Fi")
+	print("Network Config:", wlan.ifconfig())
+	WIFI_LED_LIGHT.value(1)  
+
+connect_wifi()
 
 dht_sensor = dht.DHT11(machine.Pin(4))  
 
